0f34024649b1eb52f2ad1026f995a52643c4d864
tebogo60 <tjjmalebana@gmail.com>
2025-04-14 13:21:20 +0200

    update footer ui

---
+++ Application/views/includes/footer.php
@@ -0,0 +1,91 @@
+<footer class="text-light">
+    <div class="container text-md-left">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 g-4">
+
+            <div class="col-md-2">
+                <h6 class="text-uppercase fw-bold mb-3">Documentation</h6>
+                <ul class="list-unstyled footer-links">
+                    <li><a href="#" class="text-light">Content Removals</a></li>
+                    <li><a href="#" class="text-light">POPIA Compliance</a></li>
+                    <li><a href="#" class="text-light">Privacy Policy</a></li>
+                    <li><a href="#" class="text-light">Terms & Conditions</a></li>
+                    <li><a href="#" class="text-light">Termination Policy</a></li>
+                    <li><a href="#" class="text-light">Refund Policy</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-2">
+                <h6 class="text-uppercase fw-bold mb-3">Site Navigation</h6>
+                <ul class="list-unstyled footer-links">
+                    <li><a href="#" class="text-light">Home Page</a></li>
+                    <li><a href="#" class="text-light">About Us</a></li>
+                    <li><a href="#" class="text-light">Contact Us</a></li>
+                    <li><a href="#" class="text-light">Register For Membership</a></li>
+                    <li><a href="#" class="text-light">Create An Account</a></li>
+                    <li><a href="#" class="text-light">Forgot My Password</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-2">
+                <h6 class="text-uppercase fw-bold mb-3">Useful Links</h6>
+                <ul class="list-unstyled footer-links">
+                    <li><a href="#" class="text-light">Library Listings</a></li>
+                    <li><a href="#" class="text-light">How it works</a></li>
+                    <li><a href="#" class="text-light">Member Dashboard</a></li>
+                    <li><a href="#" class="text-light">My Account</a></li>
+                    <li><a href="#" class="text-light">Help Center</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3">
+                <h6 class="text-uppercase fw-bold mb-3">Resources Guides</h6>
+                <ul class="list-unstyled footer-links">
+                    <li><a href="#" class="text-light">Guide - How to register</a></li>
+                    <li><a href="#" class="text-light">Guide - How to add a listing</a></li>
+                    <li><a href="#" class="text-light">Guide - How to reset a password</a></li>
+                    <li><a href="#" class="text-light">Guide - How to make a payment</a></li>
+                    <li><a href="#" class="text-light">Guide - How to Create A PayFast account</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3">
+                <h6 class="text-uppercase fw-bold mb-3">Contact Information</h6>
+                <div class="footer-links">
+                    <p><strong>Headquarters:</strong><br>
+                        13th Floor, Illovo Point,<br>
+                        68 Melville Rd, Illovo, Sandton, 2B6</p>
+                    <p><strong>Enquiries:</strong><br>
+                        +27 67 852 3593 (MON to FRI 9am–6pm)</p>
+                    <p><a href="mailto:admin@sabooksonline.co.za" class="text-light">admin@sabooksonline.co.za</a></p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row mt-4 align-items-center">
+            <div class="col-md-6 d-flex flex-wrap gap-2 justify-content-center justify-content-md-start mb-2 mb-md-0">
+                <div class="me-2">
+                    <select class="btn btn-white">
+                        <option>
+                            ENGLISH
+                        </option>
+                    </select>
+                </div>
+                <div>
+                    <select class="btn btn-white">
+                        <option>ZAR</option>
+                    </select>
+                </div>
+            </div>
+            <div class="footer-links col-md-6 text-center text-md-end">
+                <small>
+                    <a href="">Terms and conditions</a>
+                    |
+                    <a href="">Privacy</a>
+                    | &copy; SA Books Online</small>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<div style="width: 100%;height: 20px;background: url(../../../img/brand/02.jpg);background-size:contain;"></div>
