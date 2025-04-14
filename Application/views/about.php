8ce3a84fec2a70587ee61939f3f8dfd8c7ca056c
tebogo60 <tjjmalebana@gmail.com>
2025-04-14 19:59:35 +0200

    refactor - about page

--- Application/views/about.php
+++ Application/views/about.php
@@ -1,197 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
-
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="SA Books Online, South African Books, South African Literature, SA Literature">
-	<meta name="keywords" content="SA Books Online, South African Books, South African Literature, SA Literature">
-    <meta name="author" content="SA Books Online">
-    <title>About SA Books Online, South African Books Online, South African Books, South African Literature, SA Literature, The Gateway To South African Literature</title>
-
-    <!-- Favicons-->
-    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
-    <link rel="apple-touch-icon" type="image/x-icon" href="img/favicon.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/favicon.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/favicon.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/favicon.png">
-
-    <!-- GOOGLE WEB FONT -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com/">
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="anonymous">
-    <link rel="preload" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&amp;display=swap" as="fetch" crossorigin="anonymous">
-    <script type="text/javascript">
-    !function(e,n,t){"use strict";var o="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&amp;display=swap",r="__3perf_googleFonts_c2536";function c(e){(n.head||n.body).appendChild(e)}function a(){var e=n.createElement("link");e.href=o,e.rel="stylesheet",c(e)}function f(e){if(!n.getElementById(r)){var t=n.createElement("style");t.id=r,c(t)}n.getElementById(r).innerHTML=e}e.FontFace&&e.FontFace.prototype.hasOwnProperty("display")?(t[r]&&f(t[r]),fetch(o).then(function(e){return e.text()}).then(function(e){return e.replace(/@font-face {/g,"@font-face{font-display:swap;")}).then(function(e){return t[r]=e}).then(f).catch(a)):a()}(window,document,localStorage);
-    </script>
-
-    <!-- BASE CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
-    <!-- SPECIFIC CSS -->
-    <link href="css/contacts.css" rel="stylesheet">
-
-    <!-- SPECIFIC CSS -->
-    <link href="css/listing.css" rel="stylesheet">
-
-    <!-- YOUR CUSTOM CSS -->
-    <link href="css/custom.css" rel="stylesheet">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-V7MRDHEHSZ"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
-
-	  gtag('config', 'G-V7MRDHEHSZ');
-	  gtag('config', 'AW-11379832900');
-	</script>
-
-</head>
+<?php
+require_once __DIR__ . "/includes/header.php";
+?>
 
 <body>
-				
-	<?php include 'includes/header-internal.php';?>
+	<?php require_once __DIR__ . "/includes/nav.php"; ?>
 
-	<main class="breaker creator-mobile">
-		<div class="hero_single inner_pages background-image" data-background="url(img/bg_4.jpg)" style="height: 40vh !important;">
-			<div class="opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.6)">
-				<div class="container">
-					<div class="row justify-content-center">
-						<div class="col-xl-9 col-lg-10 col-md-8">
-							<h1>About SA Books Online</h1>
-							<p>The Gateway to South African Literature</p>
-							<a href="https://sabooksonline.co.za/pricing-plans" class="btn_1 gradient btn_scroll">Create Membership Account</a>
-						</div>
-					</div>
-					<!-- /row -->
+	<div class="jumbotron jumbotron-md">
+		<div class="container h-100 d-flex flex-column justify-content-end py-5">
+			<div class="jumbo-details">
+				<h1 class="display-4">About <b>SABooksOnline</b></h1>
+				<p class="lead mb-4">The Gateway to South African Literature</p>
+				<div class="d-flex justify-content-start">
+					<a class="btn btn-red me-2" href="#" role="button">
+						Create Membership Account
+						<i class="fas fa-arrow-right"></i>
+					</a>
 				</div>
 			</div>
 		</div>
-		<!-- /hero_single -->
-
-		<div class="bg_gray">
-			<div class="container margin_30_40">
-				<div class="main_title center">
-				    
-				    <h2>The Gateway to South African Literature</h2><br>
-
-					<span><em></em></span>
-				</div>
-
-				<p class="">SA Books Online is South Africa's first book repository that is positioning itself as the primary platform for everything that is and connects to South African literature. Founded in 2021, South African Books Online Pty Ltd offers marketing services to all South African literature products in all various formats. We are a 100% black youth-owned organization. (B-BBEE level 1).</p>
-				<p>
-The agenda around the documentation of South African literature contributes to the global library that fills particular gaps in history’s timeline whether fact or fiction which is significantly crucial, that SA Books Online has stepped up and taken responsibility as the go-to platform for everything that is South African literature. SA Books Online’s purpose is to give easy access to South African literature and content directly to the consumers of South African literature onto one platform and redirect specifically to the owners of the literary content. The intention behind this is to make the South African literature industry more lucrative for all the parties involved.
-</p>
-
-<p>
-The rich history of this country before colonization, during the invasion, in the dark times of apartheid right to the current rainbow years of a worldly admired democracy, and whenever the future may place this country, it is an important study to the understanding of human behaviour by both its citizens and the global community. As such, it is vital to make access to its literary works in all formats from political, and socio-economic activities right up to the different facets of culture lifestyle, architectural and educational developments as well as the fictional works that vividly demonstrate the evolving imaginations of the minds that are being groomed by this land. The historic footprint of SA Books Online is not only exclusive to present times, but will be enjoyed and appreciated by forthcoming generations.
-</p>
-
-<p>
-“Without a doubt, history is dictated by those who document it, thus for too long South African history has been scattered resulting in great difficulty to trace or retrace it. That was until SA Books Online, that is why we exist, to correct an unnoticed injustice to our land. As important as it is to document information, it is equally important that it is easily accessed.”
-</p>
-<p>- Founder & MD of SA Books Online, Sbusiso Manqa</p>
-
-
-				<div class="d-flex justify-content-center"><a href="https://sabooksonline.co.za/pricing-plans" class="btn_1 gradient btn_scroll text-center">Create Your Membership</a></div>
-
-
-
-
-			</div>
-			<!-- /container -->
-
-
-		</div>
-		<!-- /bg_gray -->
-		
-	</main>
-	<!-- /main -->
-
-	<?php include 'includes/footer.php';?>
-
-	
-	
-	<!-- Sign In Modal -->
-	<div id="sign-in-dialog" class="zoom-anim-dialog mfp-hide">
-		<div class="modal_header">
-			<h3>Sign In</h3>
-		</div>
-		<form>
-			<div class="sign-in-wrapper">
-				<a href="#0" class="social_bt facebook">Login with Facebook</a>
-				<a href="#0" class="social_bt google">Login with Google</a>
-				<div class="divider"><span>Or</span></div>
-				<div class="form-group">
-					<label>Email</label>
-					<input type="email" class="form-control" name="email" id="email">
-					<i class="icon_mail_alt"></i>
-				</div>
-				<div class="form-group">
-					<label>Password</label>
-					<input type="password" class="form-control" name="password" id="password" value="">
-					<i class="icon_lock_alt"></i>
-				</div>
-				<div class="clearfix add_bottom_15">
-					<div class="checkboxes float-start">
-						<label class="container_check">Remember me
-						  <input type="checkbox">
-						  <span class="checkmark"></span>
-						</label>
-					</div>
-					<div class="float-end"><a id="forgot" href="javascript:void(0);">Forgot Password?</a></div>
-				</div>
-				<div class="text-center">
-					<input type="submit" value="Log In" class="btn_1 full-width mb_5">
-					Don’t have an account? <a href="register.html">Sign up</a>
-				</div>
-				<div id="forgot_pw">
-					<div class="form-group">
-						<label>Please confirm login email below</label>
-						<input type="email" class="form-control" name="email_forgot" id="email_forgot">
-						<i class="icon_mail_alt"></i>
-					</div>
-					<p>You will receive an email containing a link allowing you to reset your password to a new preferred one.</p>
-					<div class="text-center"><input type="submit" value="Reset Password" class="btn_1"></div>
-				</div>
-			</div>
-		</form>
-		<!--form -->
 	</div>
-	<!-- /Sign In Modal -->
-	
-	<!-- COMMON SCRIPTS -->
-    <script src="js/common_scripts.min.js"></script>
-<script src="js/custom.js"></script>
-    <script src="js/common_func.js"></script>
-    <script src="assets/validate.js"></script>
 
-    <!-- SPECIFIC SCRIPTS -->
-    <script src="js/sticky_sidebar.min.js"></script>
-    <script src="js/specific_listing.js"></script>
+	<div class="container py-5">
+		<div class="mb-4">
+			<h1 class="section-title">The Gateway to South African Literature</h1>
+		</div>
 
-    <script>
-	var countries = [ <?php
-                                    //DATABASE CONNECTIONS SCRIPT
-    include 'includes/database_connections/sabooks.php';
-    $sql = "SELECT * FROM posts WHERE STATUS = 'active';";
-    //$sql = "SELECT * FROM posts WHERE TYPE = '$name' AND CATEGORY LIKE '%$name%' OR DESCRIPTION LIKE '%$name%' ORDER BY ID DESC;";
-    $result = mysqli_query($conn, $sql);
-    $result = mysqli_query($conn, $sql);
-        if(mysqli_num_rows($result) == false){
-    }else{
-        while($row = mysqli_fetch_assoc($result)) {
+		<p>
+			SA Books Online is South Africa's first book repository that is positioning itself as the primary platform for everything that is and connects to South African literature. Founded in 2021, South African Books Online Pty Ltd offers marketing services to all South African literature products in all various formats. We are a 100% black youth–owned organization. (B–BBEE level 1).
+		</p>
 
-        echo '"'.$row["TITLE"].'",';
-        }
-    }
-    ?> " "];
-	</script>
+		<p>
+			The agenda around the documentation of South African literature contributes to the global library that fills particular gaps in history’s timeline whether fact or fiction which is significantly crucial, that SA Books Online has stepped up and taken responsibility as the go-to platform for everything that is South African literature. SA Books Online’s purpose is to give easy access to South African literature and content directly to the consumers of South African literature onto one platform and redirect specifically to the owners of the literary content. The intention behind this is to make the South African literature industry more lucrative for all the parties involved.
+		</p>
 
-	<script src="js/custom.js"></script>
+		<p>
+			The rich history of this country before colonization, during the invasion, in the dark times of apartheid right to the current rainbow years of a worldly admired democracy, and whenever the future may place this country, it is an important study to the understanding of human behaviour by both its citizens and the global community. As such, it is vital to make access to its literary works in all formats from political, and socio-economic activities right up to the different facets of culture lifestyle, architectural and educational developments as well as the fictional works that vividly demonstrate the evolving imaginations of the minds that are being groomed by this land. The historic footprint of SA Books Online is not only exclusive to present times, but will be enjoyed and appreciated by forthcoming generations.
+		</p>
 
+		<p class="quote">
+			<em class="text-muted">
+				“Without a doubt, history is dictated by those who document it, thus for too long South African history has been scattered resulting in great difficulty to trace or retrace it. That was until SA Books Online, that is why we exist, to correct an unnoticed injustice to our land. As important as it is to document information, it is equally important that it is easily accessed.”
+			</em>
+		</p>
+
+		<p class="fw-medium">– Founder & MD of SA Books Online, Sbusiso Manqa</p>
+
+		<a class="btn btn-red me-2" href="#" role="button">
+			Create Membership Account
+			<i class="fas fa-arrow-right"></i>
+		</a>
+	</div>
+
+	<?php require_once __DIR__ . "/includes/footer.php" ?>
+
+	<?php require_once __DIR__ .  "/includes/scripts.php" ?>
 </body>
-
-</html>
