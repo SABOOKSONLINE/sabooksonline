932efc4f511ec333acab490310bef13ee1f2770d
tebogo60 <tjjmalebana@gmail.com>
2025-04-14 13:37:44 +0200

    remove (.php) on book href

--- database/models/Book.php
+++ database/models/Book.php
@@ -53,11 +53,11 @@ class Book
 
             echo '
                 <div class="book-card">
-                        <a class="book-card-cover" href="book.php?q=' . strtolower($book['CONTENTID']) . '">
+                        <a class="book-card-cover" href="book?q=' . strtolower($book['CONTENTID']) . '">
                             <img src="https://sabooksonline.co.za/cms-data/book-covers/' . $book['COVER'] . '" alt="">
                         </a>
                         <div class="book-card-info">
-                            <a class="book-card-little" href="book.php?q=' . strtolower($book['CONTENTID']) . '">
+                            <a class="book-card-little" href="book?q=' . strtolower($book['CONTENTID']) . '">
                             ' . (strlen($book['TITLE']) > 30 ? substr($book['TITLE'], 0, 30) . '...' : $book['TITLE']) . '
                             </a>
                             <span class="book-card-pub">
