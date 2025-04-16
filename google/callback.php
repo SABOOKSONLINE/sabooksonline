e3ca5b50e791fccf6219eb269706a8c9658b1385
kganya maleka <kganyamilton@gmail.com>
2025-04-16 18:39:23 +0200

    added user secret and id for google auth 2.o - staging website

--- google/callback.php
+++ google/callback.php
@@ -6,9 +6,16 @@ require_once 'vendor/autoload.php';
 use Google\Client as Google_Client;
 
 $client = new Google_Client();
-$client->setClientId('881101796322-kpqdbda7rse6thp07sfbd8fo1solaiij.apps.googleusercontent.com');
-$client->setClientSecret('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');
-$client->setRedirectUri('https://sabooksonline.co.za/google/callback.php');
+
+//for the main website
+// $client->setClientId('881101796322-kpqdbda7rse6thp07sfbd8fo1solaiij.apps.googleusercontent.com');
+// $client->setClientSecret('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');
+// $client->setRedirectUri('https://sabooksonline.co.za/google/callback.php');
+
+//for the staging website
+$client->setClientId('127479199120-jekj11lumhpkcu3vs7uh0ke64q9pt56u.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->setRedirectUri('https://11-july-2023.sabooksonline.co.za//google/callback.php');
 $client->addScope('email');
 $client->addScope('profile');
 
