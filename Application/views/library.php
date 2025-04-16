e151701bc1d19bb4a03df086f7db560c60c6ee09
kganya maleka <kganyamilton@gmail.com>
2025-04-16 13:18:33 +0200

    working on the catalogue page

--- Application/views/library.php
+++ Application/views/library.php
@@ -6,7 +6,7 @@ require_once __DIR__ . "/includes/header.php";
     <?php require_once __DIR__ . "/includes/nav.php"; ?>
 
 
-    <section class="tags">
+    <!-- <section class="tags">
         <div class="container py-5 category-container">
             <div class="d-flex flex-wrap justify-content-center">
                 <a href="#" class="category-link">Category</a>
@@ -26,7 +26,7 @@ require_once __DIR__ . "/includes/header.php";
                 <a href="#" class="category-link">Category</a>
             </div>
         </div>
-    </section>
+    </section> -->
 
     <div class="container py-4 ">
         <div class="mb-5">
@@ -41,7 +41,7 @@ require_once __DIR__ . "/includes/header.php";
             require_once __DIR__ . "/../../database/connection.php";
             require_once __DIR__ . "/../models/Book.php";
             require_once __DIR__ . "/../controllers/BookController.php";
-            allBooks($conn, 20);
+            allBooks($conn, limit: 10);
             ?>
 
         </div>
