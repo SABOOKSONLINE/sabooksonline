78d49e7eafda659cec799fe988f5e360bb5603de
kganya maleka <kganyamilton@gmail.com>
2025-04-18 14:29:15 +0200

    adding documenation in book store  model

--- Application/models/BookStoreModel.php
+++ Application/models/BookStoreModel.php
@@ -1,16 +1,39 @@
 <?php
+
+/**
+ * Class BookStoreModel
+ *
+ * Handles data operations related to bookstores.
+ */
 class BookStoreModel {
+    /**
+     * @var mysqli Database connection instance
+     */
     private $conn;
 
+    /**
+     * BookStoreModel constructor.
+     *
+     * @param mysqli $conn MySQLi database connection
+     */
     public function __construct($conn) {
         $this->conn = $conn;
     }
 
-    // Fetch bookstores by province or all
-    public function fetchBookstores($province = null) {
+    /**
+     * Fetch all bookstores, optionally filtered by province.
+     *
+     * @param string|null $province Optional province filter
+     * @return array List of bookstores as associative arrays
+     */
+    public function fetchBookstores(?string $province = null): array {
+        // Sanitize input if provided
         $province = $province ? mysqli_real_escape_string($this->conn, $province) : null;
 
+        // Base query for bookstores
         $sql = "SELECT * FROM users WHERE ADMIN_TYPE = 'book-store'";
+
+        // Add province filter if needed
         if ($province) {
             $sql .= " AND ADMIN_PROVINCE = '$province'";
         }
@@ -18,6 +41,7 @@ class BookStoreModel {
         $result = mysqli_query($this->conn, $sql);
         $bookstores = [];
 
+        // Fetch results if any
         if ($result && mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
                 $bookstores[] = $row;
