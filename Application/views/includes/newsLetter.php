d06a236f9602872c3682d083775efc9c287d88c0
tebogo60 <tjjmalebana@gmail.com>
2025-04-22 11:29:29 +0200

    update - newsletter/subscribe section added

---
+++ Application/views/includes/newsLetter.php
@@ -0,0 +1,22 @@
+<section class="sub-layout">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="fw-bold">Subscribe to our Newsletter</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, mollitia ipsum eum rerum, eos earum reiciendis quasi minima quibusdam velit vitae accusamus eaque commodi deserunt blanditiis unde repudiandae nemo perspiciatis!</p>
+            </div>
+
+            <div class="col-md-6 align-content-center">
+                <form class="" action="">
+                    <div class="input-group">
+                        <input type="text" class="form-control text-secondary" placeholder="Enter Your Email"
+                            name="subscribe" value="">
+                        <button class="btn btn-sub" type="submit">
+                            subscribe
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
