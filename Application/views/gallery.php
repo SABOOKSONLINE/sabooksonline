702bcbdffb2306ab9f1e19c8f106934d1de638a6
tebogo60 <tjjmalebana@gmail.com>
2025-04-22 11:28:31 +0200

    update - gallery page added

---
+++ Application/views/gallery.php
@@ -0,0 +1,64 @@
+<?php
+require_once __DIR__ . "/includes/header.php";
+?>
+
+<body>
+    <?php require_once __DIR__ . "/includes/nav.php"; ?>
+
+    <div class="jumbotron jumbotron-md">
+        <div class="container h-100 d-flex flex-column justify-content-end py-5">
+            <div class="jumbo-details">
+                <h1 class="display-4">Gallery</h1>
+                <p class="lead mb-4">The Gateway to South African Literature</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container py-5">
+        <div class="mb-4">
+            <h1 class="section-title">Title here...</h1>
+        </div>
+
+        <div class="row gap-3">
+            <div class="col-md-4 gallery-div">
+                <div class="gallery-img">
+                    <img src="../../cms-data/profile-images/-64a8141688736908.jpeg" alt="">
+                </div>
+            </div>
+
+            <div class="col-md-4 gallery-div">
+                <div class="gallery-img">
+                    <img src="../../cms-data/profile-images/-64a8141688736908.jpeg" alt="">
+                </div>
+            </div>
+
+            <div class="col-md-4 gallery-div">
+                <div class="gallery-img">
+                    <img src="../../cms-data/profile-images/-64a8141688736908.jpeg" alt="">
+                </div>
+            </div>
+
+            <div class="col-md-4 gallery-div">
+                <div class="gallery-img">
+                    <img src="../../cms-data/profile-images/-64a8141688736908.jpeg" alt="">
+                </div>
+            </div>
+
+            <div class="col-md-4 gallery-div">
+                <div class="gallery-img">
+                    <img src="../../cms-data/profile-images/-64a8141688736908.jpeg" alt="">
+                </div>
+            </div>
+
+            <div class="col-md-4 gallery-div">
+                <div class="gallery-img">
+                    <img src="../../cms-data/profile-images/-64a8141688736908.jpeg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require_once __DIR__ . "/includes/footer.php" ?>
+
+    <?php require_once __DIR__ .  "/includes/scripts.php" ?>
+</body>
