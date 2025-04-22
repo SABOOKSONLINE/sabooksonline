e976405356af858aaf73b5486d52dad43885b3cf
tebogo60 <tjjmalebana@gmail.com>
2025-04-22 11:30:09 +0200

    update - providers page added

---
+++ Application/views/providers.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . "/includes/header.php";
+?>
+
+<body>
+    <?php require_once __DIR__ . "/includes/nav.php"; ?>
+
+    <div class="container py-4 ">
+        <h1 class="fw-bold mb-0">Service Providers</h1>
+
+        <div class="row py-4">
+        </div>
+    </div>
+
+    <?php require_once __DIR__ . "/includes/footer.php" ?>
+
+    <?php require_once __DIR__ .  "/includes/scripts.php" ?>
+</body>
