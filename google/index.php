e2a4d47f22fb0edc79a8a144d2389d7e18e64236
kganya maleka <kganyamilton@gmail.com>
2025-04-23 13:33:05 +0200

    resolved - staging website urls callback

--- google/index.php
+++ google/index.php
@@ -8,7 +8,7 @@ use Google\Client as Google_Client;
 $client = new Google_Client();
 $client->setClientId('881101796322-kpqdbda7rse6thp07sfbd8fo1solaiij.apps.googleusercontent.com');
 $client->setClientSecret('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');
-$client->setRedirectUri('https://sabooksonline.co.za/google/callback.php');
+$client->setRedirectUri('https://11-july-2023.sabooksonline.co.za/google/callback.php');
 $client->addScope('email');
 $client->addScope('profile');
 
