d505e0f9b66079c8f6a2e64da3fa9a7a189f38c7
kganya maleka <kganyamilton@gmail.com>
2025-04-25 10:17:57 +0200

    addinf code for displaying errors

--- dashboard/views/manage_books.php
+++ dashboard/views/manage_books.php
@@ -1,8 +1,13 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
 require_once '../../includes/database_connections/sabooks.php';
 require_once '../controllers/ListingsController.php';
 
-session_start();
 $bookController = new ListingsController($conn);
 $userkey = $_SESSION['ADMIN_USERKEY'];
 $books = $bookController->listBooks($userkey);
