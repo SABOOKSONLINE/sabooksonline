f93b646e0b7e377d7be7c6cc8214a3accdb173a6
tebogo60 <tjjmalebana@gmail.com>
2025-05-04 00:05:10 +0200

    refactor(nav): navbar responsiveness, make it look good on both mobile and desktop

--- Application/views/includes/nav.php
+++ Application/views/includes/nav.php
@@ -1,62 +1,76 @@
-<div style="width: 100%;height: 20px;background: url(../../../img/brand/02.jpg);background-size:contain;"></div>
-
-<header class="navbar navbar-expand-md navbar-light bg-light py-2">
+<nav class="navbar navbar-expand-xl navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand me-2" href="/" aria-label="sabooksonline">
-            <img src="../../../public/images/sabo_logo.png" alt="sabooksonline logo" width="120">
+        <a class="navbar-brand" href="/">
+            <img src="../../../public/images/sabo_logo.png" alt="sabooksonline logo" width="90">
         </a>
 
-        <!-- Mobile Toggle Button -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <div class="d-flex order-xl-last">
+            <button class="btn border-0 me-2 navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mobileSearch">
+                <i class="fas fa-search"></i>
+            </button>
 
-        <!-- Navbar Content -->
-        <div class="collapse navbar-collapse" id="navbarContent">
-            <ul class="navbar-nav me-auto">
-                <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="fas fa-bars"></span>
+            </button>
+        </div>
 
-                <li class="nav-item"><a class="nav-link" href="/library">Library</a></li>
-
-                <!-- Community Dropdown -->
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/library">Library</a>
+                </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="communityDropdown" role="button" data-bs-toggle="dropdown">
+                    <a class="nav-link dropdown-toggle show" href="#" id="communityDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">
                         Community
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul class="dropdown-menu" data-bs-popper="static">
                         <li><a class="dropdown-item" href="/events">Events</a></li>
                         <li><a class="dropdown-item" href="/providers">Service Providers</a></li>
                         <li><a class="dropdown-item" href="/membership">Membership Pricing</a></li>
                     </ul>
                 </li>
-
-                <li class="nav-item"><a class="nav-link" href="/services">Our Services</a></li>
-                <li class="nav-item"><a class="nav-link" href="/gallery">Gallery</a></li>
-                <li class="nav-item"><a class="nav-link" href="/about">About Us</a></li>
-                <li class="nav-item"><a class="nav-link" href="/contact">Contact Us</a></li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/services">Our Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/gallery">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contact">Contact</a>
+                </li>
             </ul>
 
-            <!-- Search and Auth Buttons -->
-            <div class="d-flex">
-                <!-- Search Form -->
-                <form class="d-flex me-3" action="/library" method="GET" id="myForm">
+            <div class="d-none d-xl-flex">
+                <form class="d-flex me-3" action="/library">
                     <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search by Title, Authors or Publisher"
-                            name="k" value="<?= htmlspecialchars($_GET['k'] ?? '') ?>">
-                        <button class="btn btn-outline-secondary" type="submit">
-                            <i class="fas fa-search"></i>
-                        </button>
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     </div>
                 </form>
 
+                <div class="btn-group">
+                    <a href="#" class="btn btn-outline-danger btn-outline-red">LOGIN</a>
+                    <a href="#" class="btn btn-danger btn-red">SIGN UP</a>
+                </div>
+            </div>
 
-                <!-- Auth Buttons -->
-                <a class="btn btn-outline-red me-2" href="../../../pricing-plans.php">REGISTER</a>
-                <a class="btn btn-red" href="https://11-july-2023.sabooksonline.co.za/dashboard">
-                    <span class="me-2">LOGIN</span>
-                    <i class="fas fa-sign-in-alt"></i>
-                </a>
+            <div class="d-xl-none bg-light p-3">
+                <div class="d-grid gap-2">
+                    <a href="#" class="btn btn-outline-danger btn-outline-red">LOGIN</a>
+                    <a href="#" class="btn btn-danger btn-red">SIGN UP</a>
+                </div>
             </div>
         </div>
     </div>
-</header>
+</nav>
+
+<div class="collapse container-fluid bg-light p-3" id="mobileSearch">
+    <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    </form>
+</div>
