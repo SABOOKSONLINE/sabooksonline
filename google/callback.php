d92930a5cb15a6e60d3712fe222adf72834a9825
kganya maleka <kganyamilton@gmail.com>
2025-05-05 23:01:47 +0200

    fix: including eror debigging

--- google/callback.php
+++ google/callback.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 
 require_once 'vendor/autoload.php';
@@ -11,7 +14,7 @@ $client = new Google_Client();
 $client->setClientId('881101796322-kpqdbda7rse6thp07sfbd8fo1solaiij.apps.googleusercontent.com');
 $client->setClientSecret('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');
 
-$client->setRedirectUri('https://11-july-2023.sabooksonline.co.za/google/callback.php');
+$client->setRedirectUri('https://11-july-2023.sabooksonline.co.za/google/callback');
 $client->addScope('email');
 $client->addScope('profile');
 
