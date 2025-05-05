7361911c38b467ad9bbcfe48091996944cfe00cc
kganya maleka <kganyamilton@gmail.com>
2025-05-05 17:47:16 +0200

    refactor: login linked with ajax

--- Application/views/includes/login_action.php
+++ Application/views/includes/login_action.php
@@ -7,7 +7,6 @@ session_start();
 
 require_once __DIR__ . '/../controllers/AuthController.php';
 require_once __DIR__ . '/../../includes/database_connections/sabooks.php';
-require_once __DIR__ . '/../../google/vendor/autoload.php';
 
 $authController = new AuthController($conn);
 
