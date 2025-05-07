2f1f3c0465b860b82a0b2d087547ad92c86a0dd9
kganya maleka <kganyamilton@gmail.com>
2025-05-07 15:05:10 +0200

    feature: bookcontentupload included correct database

--- dashboard/uploadbookContent.php
+++ dashboard/uploadbookContent.php
@@ -1,5 +1,5 @@
 <?php
-include '../includes/database_connections/sabooks.php';
+require_once __DIR__ . '/../includes/database_connections/sabooks_user.php';
 session_start();
 
 if (!isset($_SESSION['ADMIN_USERKEY'])) {
