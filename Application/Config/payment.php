7c8a922d24363abde9b0554b7c173837c9fabb0b
malekamk <kganyamilton@gmail.com>
2025-05-09 09:05:52 +0200

    feature: config file for payments secrets

---
+++ Application/Config/payment.php
@@ -0,0 +1,4 @@
+<?php
+define('FLW_SECRET_KEY', 'FLWSECK_TEST-xxxxxxxxxxxxx');  // Replace with live later
+define('FLW_PUBLIC_KEY', 'FLWPUBK_TEST-xxxxxxxxxxxxx');
+define('BASE_URL', 'https://11-july-2023.sabooksonline.co.za'); // or your live domain
