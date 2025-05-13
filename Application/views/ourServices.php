eaa1a10bf99cafb13ce3fd39421c6cd308c8497d
tebogo60 <tjjmalebana@gmail.com>
2025-05-13 16:28:13 +0200

    refactor: add images - service cards

--- Application/views/ourServices.php
+++ Application/views/ourServices.php
@@ -18,6 +18,9 @@ require_once __DIR__ . "/includes/header.php";
         <div class="row">
             <div class="col-md-4 mb-3">
                 <div class="card h-100 shadow-sm">
+                    <div class="card-img">
+                        <img src="/public/images/gallery/subscription_plans.jpg" class="card-img-top" alt="...">
+                    </div>
                     <div class="card-body">
                         <h4 class="card-title">
                             Subscription Plans
@@ -33,13 +36,16 @@ require_once __DIR__ . "/includes/header.php";
 
             <div class="col-md-4 mb-3">
                 <div class="card h-100 shadow-sm">
+                    <div class="card-img">
+                        <img src="/public/images/gallery/digital_marketing.jpg" class="card-img-top" alt="...">
+                    </div>
                     <div class="card-body">
                         <h4 class="card-title">
                             Digital Marketing
                         </h4>
                         <hr />
                         <p class="card-text">
-                            Promote your books, literary events, or author brand through tailored online marketing
+                            Promote your books, library events, or author brand through tailored online marketing
                             campaigns across web and social media platforms.
                         </p>
                     </div>
@@ -48,6 +54,9 @@ require_once __DIR__ . "/includes/header.php";
 
             <div class="col-md-4 mb-3">
                 <div class="card h-100 shadow-sm">
+                    <div class="card-img">
+                        <img src="/public/images/gallery/consultaion_service.jpg" class="card-img-top" alt="...">
+                    </div>
                     <div class="card-body">
                         <h4 class="card-title">
                             Consultations
@@ -63,6 +72,9 @@ require_once __DIR__ . "/includes/header.php";
 
             <div class="col-md-4 mb-3">
                 <div class="card h-100 shadow-sm">
+                    <div class="card-img">
+                        <img src="/public/images/gallery/ebook_conversion.jpg" class="card-img-top" alt="...">
+                    </div>
                     <div class="card-body">
                         <h4 class="card-title">
                             eBook Conversion
@@ -78,6 +90,9 @@ require_once __DIR__ . "/includes/header.php";
 
             <div class="col-md-4 mb-3">
                 <div class="card h-100 shadow-sm">
+                    <div class="card-img">
+                        <img src="/public/images/gallery/audiobook_conversion.jpg" class="card-img-top" alt="...">
+                    </div>
                     <div class="card-body">
                         <h4 class="card-title">
                             Audiobook Conversion
@@ -93,13 +108,16 @@ require_once __DIR__ . "/includes/header.php";
 
             <div class="col-md-4 mb-3">
                 <div class="card h-100 shadow-sm">
+                    <div class="card-img">
+                        <img src="/public/images/gallery/event_planning.jpg" class="card-img-top" alt="...">
+                    </div>
                     <div class="card-body">
                         <h4 class="">
                             Event Planning
                         </h4>
                         <hr />
                         <p class="card-text">
-                            Let us help you plan, promote, and manage literary events, from book launches to virtual
+                            Let us help you plan, promote, and manage library events, from book launches to virtual
                             discussions.
                         </p>
                     </div>
