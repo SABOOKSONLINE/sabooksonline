3472b25ad8def118d274bf68c36b3f42a70c215b
kmaleka <kganyamilton@gmail.com>
2025-05-17 20:50:52 +0200

    fix: checkout post and clear old code

--- Application/views/checkout.php
+++ Application/views/checkout.php
@@ -1,12 +1,7 @@
 <?php 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/checkout') {
-    require_once '../config/connection.php';
-    (new CheckoutController($conn))->purchaseBook($_POST['bookId'], $_POST['userId']);
-    exit;
-}
-
 session_start();
+require_once '../config/connection.php';
 require_once __DIR__ . '/../controllers/CheckoutController.php';
 
 // Assume user is logged in and their ID is stored in session
