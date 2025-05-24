aaedba84f828e19d75bbe1b1b885977a86b13f50
tebogo60 <tjjmalebana@gmail.com>
2025-05-24 18:11:32 +0200

    refactor(footer): update footer routes

--- Application/views/includes/footer.php
+++ Application/views/includes/footer.php
@@ -2,7 +2,7 @@
     <div class="container text-md-left">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 g-4">
 
-            <div class="col-md-2">
+            <div class="col-md-3">
                 <h6 class="text-uppercase fw-bold mb-3">Documentation</h6>
                 <ul class="list-unstyled footer-links">
                     <li><a href="/content-removal" class="text-light">Content Removals</a></li>
@@ -14,30 +14,30 @@
                 </ul>
             </div>
 
-            <div class="col-md-2">
+            <div class="col-md-3">
                 <h6 class="text-uppercase fw-bold mb-3">Site Navigation</h6>
                 <ul class="list-unstyled footer-links">
                     <li><a href="/" class="text-light">Home Page</a></li>
                     <li><a href="/about" class="text-light">About Us</a></li>
                     <li><a href="/contact" class="text-light">Contact Us</a></li>
                     <li><a href="/membership" class="text-light">Register For Membership</a></li>
-                    <li><a href="#" class="text-light">Create An Account</a></li>
-                    <li><a href="#" class="text-light">Forgot My Password</a></li>
-                </ul>
-            </div>
-
-            <div class="col-md-2">
-                <h6 class="text-uppercase fw-bold mb-3">Useful Links</h6>
-                <ul class="list-unstyled footer-links">
-                    <li><a href="/library" class="text-light">Library Listings</a></li>
-                    <li><a href="#" class="text-light">How it works</a></li>
-                    <li><a href="#" class="text-light">Member Dashboard</a></li>
-                    <li><a href="#" class="text-light">My Account</a></li>
-                    <li><a href="#" class="text-light">Help Center</a></li>
+                    <li><a href="/signup" class="text-light">Create An Account</a></li>
+                    <li><a href="/forgot-password" class="text-light">Forgot My Password</a></li>
                 </ul>
             </div>
 
             <div class="col-md-3">
+                <h6 class="text-uppercase fw-bold mb-3">Useful Links</h6>
+                <ul class="list-unstyled footer-links">
+                    <li><a href="/library" class="text-light">Library Listings</a></li>
+                    <li><a href="#" class="text-light">How it works</a></li>
+                    <li><a href="/dashboards" class="text-light">Member Dashboard</a></li>
+                    <li><a href="/dashboards/profile" class="text-light">My Account</a></li>
+                    <li><a href="/contact" class="text-light">Help Center</a></li>
+                </ul>
+            </div>
+
+            <!-- <div class="col-md-3">
                 <h6 class="text-uppercase fw-bold mb-3">Resources Guides</h6>
                 <ul class="list-unstyled footer-links">
                     <li><a href="#" class="text-light">Guide - How to register</a></li>
@@ -46,7 +46,7 @@
                     <li><a href="#" class="text-light">Guide - How to make a payment</a></li>
                     <li><a href="#" class="text-light">Guide - How to Create A PayFast account</a></li>
                 </ul>
-            </div>
+            </div> -->
 
             <div class="col-md-3">
                 <h6 class="text-uppercase fw-bold mb-3">Contact Information</h6>
