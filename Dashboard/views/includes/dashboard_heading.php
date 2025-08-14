0d060f30ae20092a705bed0a5198a09de3cc98a7
tebogo60 <tjjmalebana@gmail.com>
2025-08-14 21:43:51 +0200

    fix(dashboard): align heading section content side-by-side with centered items

--- Dashboard/views/includes/dashboard_heading.php
+++ Dashboard/views/includes/dashboard_heading.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
 function renderHeading($heading, $lead, $buttonUrl = "", $buttonInnerText = "", $printToPdf = false)
 { ?>
-    <div class="row align-items-center justify-content-between border-bottom pb-3 mb-4">
-        <div class="col">
+    <div class="d-flex justify-content-between align-items-center">
+        <div>
             <h1 class="display-5 fw-bold"><?= $heading ?></h1>
             <p class="lead text-muted"><?= $lead ?></p>
         </div>
 
+        <div>
             <?php if (!empty($buttonUrl) && !empty($buttonInnerText)): ?>
                 <a href="<?= $buttonUrl ?>" class="btn btn-outline-dark shadow-sm">
                     <?= $buttonInnerText ?>
@@ -15,8 +16,9 @@ function renderHeading($heading, $lead, $buttonUrl = "", $buttonInnerText = "", 
 
             <?php if ($printToPdf): ?>
                 <button id="printPDF" class="btn btn-outline-dark shadow-sm">
-                     Print Screen                
+                    Print Screen
                 </button>
             <?php endif; ?>
+        </div>
     </div>
 <?php } ?>
