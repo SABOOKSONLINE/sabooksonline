7a991f200779ca2cbd932781ed02498126828282
tebogo60 <tjjmalebana@gmail.com>
2025-08-22 15:14:00 +0200

    feat(session): store full current page URL in session

--- Application/views/util/urlRedirect.php
+++ Application/views/util/urlRedirect.php
@@ -1,6 +1,13 @@
 <?php
-$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'
-    || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$protocol = 'http://';
+if (
+    (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ||
+    $_SERVER['SERVER_PORT'] == 443 ||
+    (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+        $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+) {
+    $protocol = 'https://';
+}
 
 $currentUrl = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
