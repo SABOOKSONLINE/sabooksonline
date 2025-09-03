9a1e3f0743913e9e4cef927f2418db72f7872260
tebogo60 <tjjmalebana@gmail.com>
2025-09-03 15:07:05 +0200

    fix(mailer): update mail host and smtp encryption

--- Application/views/auth/default_mailer.php
+++ Application/views/auth/default_mailer.php
@@ -9,17 +9,17 @@ function sendEmail($to, $link, $message)
     $mail = new PHPMailer(true);
     try {
         $mail->isSMTP();
-        $mail->Host = 'ns1.host-h.net';
+        $mail->Host = 'www22.jnb2.host-h.net';
         $mail->SMTPAuth = true;
         $mail->Username = 'no-reply@sabooksonline.co.za';
         $mail->Password = '75o783F0O4L79o';
-        $mail->SMTPSecure = 'tls';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port = 465;
 
         $mail->setFrom('no-reply@sabooksonline.co.za', 'SA Books Online');
         $mail->addAddress($to);
-        $mail->Subject = 'Verify your email';
         $mail->isHTML(true);
+        $mail->Subject = 'Verify your email';
         $mail->Body = "$message <a href='$link'>$link</a>";
 
         $mail->send();
