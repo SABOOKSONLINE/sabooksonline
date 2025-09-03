a4f62f0e560b676c6b3db77781c085b0b3c115db
tebogo60 <tjjmalebana@gmail.com>
2025-09-03 16:23:54 +0200

    feat(admin): add admin dashboard conn

---
+++ Admin/Core/Conn.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$serverName = "sql58.jnb1.host-h.net";
+$username = "sabooksonline";
+$password = "slTFvaj07dNY6Ke";
+$primaryDb = "sabookso_db";
+
+// Enable exceptions for mysqli
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+try {
+    // create connection to primaryDb
+    $conn = new mysqli($serverName, $username, $password, $primaryDb);
+    $conn->set_charset("utf8mb4");
+} catch (mysqli_sql_exception $e) {
+    // Centralized error logging
+    error_log("Database Connection Error: " . $e->getMessage());
+    die("Database Connection Error: " . $e->getMessage());
+}
