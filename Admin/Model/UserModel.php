f03fc8503dba5711561b74fbf2bb19d792b5555b
tebogo60 <tjjmalebana@gmail.com>
2025-09-07 00:39:59 +0200

    remove User model

---
+++ Admin/Model/UserModel.php
@@ -0,0 +1,49 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . "/../Core/Model.php";
+
+class UserModel extends Model
+{
+    public function getAdminUser(string $email): array
+    {
+        return $this->fetchPrepared(
+            "SELECT * FROM admin_users 
+            WHERE email = ?",
+            "s",
+            [$email]
+        );
+    }
+
+    public function getAllAdmins(): array
+    {
+        return $this->fetchAll("SELECT * FROM admin_users");
+    }
+
+    public function getAllUsers(): array
+    {
+        return $this->fetchAll("SELECT * FROM users");
+    }
+
+    public function countUsers(): array
+    {
+        $result = $this->fetchAll("SELECT COUNT(*) AS total FROM users");
+        return $result[0];
+    }
+
+    public function countSubscribers(): array
+    {
+        $result = $this->fetchAll("SELECT COUNT(*) AS total 
+                                    FROM users 
+                                    WHERE LOWER(subscription_status) IN ('royalties', 'premium', 'pro');");
+        return $result[0];
+    }
+
+    public function grossSubscriptionIncome(): array
+    {
+        $result = $this->fetchAll("SELECT SUM(amount_paid) AS gross FROM payment_plans");
+        return $result[0];
+    }
+}
