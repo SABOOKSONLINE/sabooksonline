00e7298102e0f67e1872375cef0ec9987fe4e67b
tebogo60 <tjjmalebana@gmail.com>
2025-09-07 00:34:24 +0200

    feat(admin): update structure to use MVC for admin dashboard

--- Admin/index.php
+++ Admin/index.php
@@ -1,5 +1,17 @@
 <?php
-$title = "Manage Users";
-ob_start();
-$content = ob_get_clean();
-require __DIR__ . "/View/layouts/base.php";
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require __DIR__ . "/Core/Conn.php";
+require __DIR__ . "/Controller/HomeController.php";
+
+$uri = $_SERVER['REQUEST_URI'];
+
+$controller = new HomeController($conn);
+
+if ($uri === "/admin") {
+    $controller->index();
+} else {
+    http_response_code(404);
+    echo "404 Page Not Found";
+}
