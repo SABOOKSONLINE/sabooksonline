45aec3eb9ee79d1b076d1afba3b297e2006015c2
tebogo60 <tjjmalebana@gmail.com>
2025-09-09 16:08:57 +0200

    feat(ui): add renderBookCards function for displaying book cards

--- Admin/View/layouts/cards/bkCard.php
+++ Admin/View/layouts/cards/bkCard.php
@@ -1,8 +1,8 @@
 <?php
-function renderBookCards($books, $viewOnly = false)
+function renderBookCards($books, $viewOnly = false, $section = "")
 {
 ?>
-    <div class="row">
+    <div class="row position-relative">
         <div class="scroll-row-wrapper">
             <div class="scroll-row p-2">
                 <?php foreach ($books as $book): ?>
@@ -33,13 +33,27 @@ function renderBookCards($books, $viewOnly = false)
                         </div>
                     </div>
                 <?php endforeach ?>
+                <?php if (!$viewOnly): ?>
+                    <button
+                        id="books_table_<?= $section ?>"
+                        class="books_table_open btn btn-dark rounded-circle d-flex align-items-center justify-content-center 
+           position-absolute top-50 end-0 translate-middle-y me-3"
+                        style="width: 50px; height: 50px; z-index: 2;"
+                        data-bs-toggle="modal"
+                        data-bs-target="#modal_<?= str_replace(' ', '_', $section) ?>"
+                        type="button">
+                        <i class="fas fa-plus"></i>
+                    </button>
+                <?php endif ?>
             </div>
 
-            <div id="scrollHint" class="scroll-hint d-flex align-items-center text-muted">
-                <i class="fas fa-angle-left me-2"></i>
-                <span>Scroll to see more books</span>
-                <i class="fas fa-angle-right ms-2"></i>
-            </div>
+            <?php if (count($books) >= 4): ?>
+                <div id="scrollHint" class="scroll-hint d-flex align-items-center text-muted justify-content-end mt-2">
+                    <i class="fas fa-angle-left me-2"></i>
+                    <span>Scroll to see more books</span>
+                    <i class="fas fa-angle-right ms-2"></i>
+                </div>
+            <?php endif ?>
         </div>
     </div>
 <?php
