42d8f15d1d8d297ccda08c64d8080de4ec4a4226
tebogo60 <tjjmalebana@gmail.com>
2025-10-14 14:35:54 +0200

    refactor: add banners custom javascript file

---
+++ public/js/banners.js
@@ -0,0 +1,15 @@
+// const stickyBanner = document.querySelector("#sticky-banner");
+
+// window.addEventListener("scroll", () => {
+//     const y = window.scrollY;
+//     if (y >= 80) {
+//         stickyBanner.classList.add("fixed-top");
+//         stickyBanner.style.left = "0";
+//         stickyBanner.style.right = "0";
+//     } else {
+//         stickyBanner.classList.remove("fixed-top");
+//         stickyBanner.style.left = "";
+//         stickyBanner.style.right = "";
+//     }
+//     console.log(y);
+// });
