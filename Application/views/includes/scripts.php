e9900fc2f2f11bd801d4412320a3e23418ec2783
tebogo60 <tjjmalebana@gmail.com>
2025-10-17 11:11:46 +0200

    fix: store JS files locally instead of using external links

--- Application/views/includes/scripts.php
+++ Application/views/includes/scripts.php
@@ -1,8 +1,8 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="/public/js/bootstrap.bundle.min.js"></script>
 <script src="/public/js/copy_field.js"></script>
 <script src="/public/js/main-script.js"></script>
 <script src="/public/js/pricing-toggle.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="/public/js/jquery-3.6.0.min.js"></script>
 <script src="/public/js/auth_form.js"></script>
 <script src="/public/js/banners.js"></script>
 
