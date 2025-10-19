9861794177e25b714dcc506a08148d543f4a05ae
tebogo60 <tjjmalebana@gmail.com>
2025-10-19 15:20:01 +0200

    feat(admin): add sticky and page banner sections to home page view

--- Admin/View/pages/homePage.php
+++ Admin/View/pages/homePage.php
@@ -3,6 +3,8 @@ include __DIR__ . "/../layouts/pageHeader.php";
 include __DIR__ . "/../layouts/sectionHeader.php";
 include __DIR__ . "/../layouts/cards/bkCard.php";
 include __DIR__ . "/../layouts/tables/bTable.php";
+include __DIR__ . "/../layouts/banners/stickyBanner.php";
+include __DIR__ . "/../layouts/banners/mBanner.php";
 
 require_once __DIR__ . "/../../Helpers/sessionAlerts.php";
 
@@ -20,6 +22,21 @@ renderAlerts();
 <?php
 $listings = $data["listings"];
 $allBooks = $data["books"];
+$banners = $data["banners"];
+
+renderSectionHeader(
+    "Sticky Banner",
+    "",
+);
+
+echo renderStickyBannerSlider($banners["sticky_banners"]);
+
+renderSectionHeader(
+    "Page Banner",
+    "",
+);
+
+echo renderImageCarouselBanner($banners["page_banners"], "featuredBookCarousel");
 
 $booksBySection = [];
 foreach ($listings as $book) {
