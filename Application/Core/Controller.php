6547b83ccd240c629c32e850b3f0cb0131ba17fd
tebogo60 <tjjmalebana@gmail.com>
2025-10-20 22:13:39 +0200

    feat: create base Controller class with render support

---
+++ Application/Core/Controller.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class Controller
+{
+    protected mysqli $conn;
+
+    public function __construct(mysqli $conn)
+    {
+        $this->conn = $conn;
+    }
+
+    protected function render(string $viewPath, array $data = [])
+    {
+        extract($data);
+        require __DIR__ . "/../views/"  . $viewPath . ".php";
+    }
+}
