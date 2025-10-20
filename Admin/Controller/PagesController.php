096d906e63607aad589f2811060d7acd8b7757f0
tebogo60 <tjjmalebana@gmail.com>
2025-10-20 19:20:05 +0200

    feat: add banner management methods to controller

--- Admin/Controller/PagesController.php
+++ Admin/Controller/PagesController.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . "/../Core/Controller.php";
+require_once __DIR__ . "/../Model/BookModel.php";
 require_once __DIR__ . "/../Model/BannersModel.php";
 
 class PagesController extends Controller
@@ -45,5 +46,35 @@ class PagesController extends Controller
     public function deleteListing(string $publicKey): int
     {
         return $this->bookModel->deleteListing($publicKey);
+    }
+
+    public function addStickyBanner($data): int
+    {
+        return $this->bannerModel->addStickyBanner($data);
+    }
+
+    public function deleteStickyBanner($id): int
+    {
+        return $this->bannerModel->removeStickyBanner($id);
+    }
+
+    public function addPageBanner($data): int
+    {
+        return $this->bannerModel->addPageBanner($data);
+    }
+
+    public function deletePageBanner($id): int
+    {
+        return $this->bannerModel->removePageBanner($id);
+    }
+
+    public function addPopupBanner($data): int
+    {
+        return $this->bannerModel->addPopupBanner($data);
+    }
+
+    public function deletePopupBanner($id): int
+    {
+        return $this->bannerModel->removePopupBanner($id);
     }
 }
